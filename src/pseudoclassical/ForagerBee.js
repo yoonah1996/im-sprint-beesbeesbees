262b775be20e5ad73c3b343247709f79ec1df1e2
yoonah1996 <yoonah1996@gmail.com>
2020-02-18 20:02:40 +0900

    수정완료

--- src/pseudoclassical/ForagerBee.js
+++ src/pseudoclassical/ForagerBee.js
@@ -1,6 +1,7 @@
 var Bee = require('./Bee');
 
 var ForagerBee = function () {
+    Bee.call(this);
     this.age = 10;
     this.job = 'find pollen';
     this.canFly = true;
--- src/pseudoclassical/HoneyMakerBee.js
+++ src/pseudoclassical/HoneyMakerBee.js
@@ -1,6 +1,7 @@
 var Bee = require('./Bee');
 
 var HoneyMakerBee = function () {
+    Bee.call(this);
     this.age = 10;
     this.job = 'make honey';
     this.honeyPot = 0;
